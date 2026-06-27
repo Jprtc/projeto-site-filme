53726055487396c2fee829f63b0a54a3232003ed
Rafael <rafael.p.almeida8@aluno.senai.br>
2026-06-26 21:49:27 -0300

    Rafael

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css'
 import Footer from './Widgets/Footer/Footer'
 import Header from './Widgets/Header/header'
 import MainBanner from './Widgets/MainBanner/MainBanner'
+import BannerPrincipal from './Widgets/BannerPrincipal/BannerPrincipal'
+import CardOferta from './Widgets/CardOferta/CardOferta'
+import Modal from './Widgets/Modal/Modal'
 
 function App() {
 

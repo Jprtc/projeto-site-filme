1451209bb01483b92454e545395380969c439a3a
Lorraine Barcelos <lorraineebarcelos@gmail.com>
2026-07-02 20:37:36 -0300

    ajusta projeto de teste

--- src/Widgets/Header/Header.jsx
+++ src/Widgets/Header/Header.jsx
@@ -1,11 +1,3 @@
-import styles from "./Header.module.css";
-import logo from "../../assets/netflixlogo.png";
-function Header() {
-  return (
-    <header className={styles.headerContainer}>
-      <img src={logo} alt="logo" className={styles.logo} />
-    </header>
-  );
+export default function Header() {
+  return null;
 }
-
-export default Header;
